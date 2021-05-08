8a2b04e30e288dfff4a049221449e78dd419dbce
saraalshater1 <>
2021-05-08 21:21:51 +0000

    added css

--- app.js
+++ app.js
@@ -1,45 +1,73 @@
-// var backGround=prompt("GreenStream is a Green website but you can change to you Favorite color, Your color is: "); 
-
-// if (backGround == "blue"){
-//   document.write("<body style="backGround-color:blue;"></body>");
-// }else{
-//   document.write("<body style="background-color: #369c58;"></body>");
-// }
-
-var quickMsg=confirm("This is a Green website but you can change it to your favorite color :D ");
-var favColor=prompt("What is your Favorite color?");
- 
-
-if (quickMsg = true) {
-    if (favColor == "blue") {
-         document.body.style.backgroundColor = "#36669c";
-    } else if(favColor == "red"){
-      document.body.style.backgroundColor = "#9c3636";
-    } else if (favColor == "yellow"){
-      document.body.style.backgroundColor = "#c2c441";
-    } else if (favColor == "purple"){
-      document.body.style.backgroundColor = "#683991";
-    } else  {
-    alert("You pressed Cancel!");   
-    }
-} 
 
 
+var quickMsg = confirm("This is a Green website but you can change it to your favorite color :D ");
+var favColor;
 
-//  var  favMusic=
+if (quickMsg == true) {
+  favColor = prompt("Please choose one (blue,red,yellow,purple,green)");
+} else {
+  alert("You clicked on cancel!");
+}
 
-//  if (radioNum > 1){
-//    console.log("Error");
-//  } else if (radioNum <= 5){
-//    console.log("Not included");
-//  }else if (radioNum === 4){
-//    console.log("No error");
-//  }
+if (favColor == "blue") {
+  document.body.style.backgroundColor = "#36669c";
+} else if (favColor == "red") {
+  document.body.style.backgroundColor = "#9c3636";
+} else if (favColor == "yellow") {
+  document.body.style.backgroundColor = "#c2c441";
+} else if (favColor == "purple") {
+  document.body.style.backgroundColor = "#683991";
+} else {
+  document.body.style.backgroundColor = "#3d9c64";
+}
 
 
 
 
+var favPodcast = prompt('Choose an area to listen to their podcast: middleEast - southEastAsia - europe - worldWide ? ')
+
+while (favPodcast !== "middleEast" && favPodcast !== "southEastAsia" && favPodcast !== "europe" && favPodcast !== "worldWide") {
+  favPodcast = prompt('only choose one of these areas! ');
+}
+
+if (favPodcast == "worldWide") {
+  document.write('<h3 style="color: white;text-align:center;font-size: 40px;">For a global podcast we suggest you to listen to <a href="#worldwidefm">Worldwide Fm</a></h3>');
+} else if (favPodcast == "middleEast") {
+  document.write('<h3 style="color: white;text-align:center;font-size: 40px;">For a podcast from the Middle east we suggest you to listen to <a href="#middleEast">Radio alhara</a></h3>');
+} else if (favPodcast == "europe") {
+  document.write(
+    '<h3 style="color: white;text-align:center;font-size: 40px;">For a podcast from Europe we suggest you to listen to <a href="#europe">Kiosk radio</a></h3>'
+  );
+} else if (favPodcast == "southEastAsia") {
+  document.write(
+    '<h3 style="color: white;text-align:center;font-size: 40px;">For a podcast from Southeast Asia we suggest you to listen to <a href="#asia">Paddygroves</a></h3>'
+  );
+}
+
+var radio = prompt("How many songs you listen to per day from 1 to 4 ?");
+
+
+if (radio > 4) {
+  radio = 4;
+}
+
+// for (var i = 0; i < radio; i++) {
+//   document.write('<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Circle-icons-radio.svg/1024px-Circle-icons-radio.svg.png" width="200px">');
+// } 
+
+
+for (var i = 1; i <= radio; i++) {
+    document.write('<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Circle-icons-radio.svg/1024px-Circle-icons-radio.svg.png" width="100px">'+ i );
+  } 
 
 
 
- 
+
+
+
+
+
+
+
+
+
